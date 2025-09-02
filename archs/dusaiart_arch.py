12c8f6035a23ba742ba020608c5dbfa3aaf4376e
Jelle De Loecker <jelle@elevenways.be>
2025-09-02 21:31:19 +0200

    🐛 Fix dusa import

--- archs/dusaiart_arch.py
+++ archs/dusaiart_arch.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../..')
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from basicsr.utils.registry import ARCH_REGISTRY
 from .iart_arch import IART
 from integrate_dusa_iart import replace_window_attention_with_dusa
